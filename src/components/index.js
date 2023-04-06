a64a59d063e05db26d0a2a65afa9769bbe69006e
adela miclea <adelamiclea709@yahoo.com>
2023-04-06 12:46:13 +0300

    wowwwwwwwww

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
 import Logo from "../components/Logo";
 import FormRow from "./FormRow";
 import Alert from "./Alert";
+import BigSidebar from "./BigSidebar";
+import Navbar from "./Navbar";
+import SmallSidebar from "./SmallSidebar";
 
-
-export {Logo,FormRow,Alert};
+export {Logo,FormRow,Alert,BigSidebar,SmallSidebar,Navbar};
